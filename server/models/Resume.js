b0540ce819c4a23f6d464e2d197e45a15e4b8035
jakecashion <jakecashion98@gmail.com>
2025-07-24 16:34:02 -0400

    Added Resume schema based on Harvard format
    Includes fields for contact info, experience, education, skills, certifications, and awards.

--- server/models/Resume.js
+++ server/models/Resume.js
@@ -2,4 +2,49 @@ const mongoose = require('mongoose')
 
 const resumeSchema = new mongoose.Schema({
     userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true},
+    title: {
+        type: String,
+        default: 'Untitled Resume'
+    },
+    fullname: String,
+    email: String,
+    phone: String,
+    address: String,
+    summary: String, //Optional field
+
+    education: [
+        {
+            school: String,
+            degree: String,
+            startDate: Date,
+            location: String,
+            honors: String, // Optional
+        }
+    ],
+
+    experience: [
+        {
+        title: {type: String, required: true},
+        company: {type:String, required: true},
+        location: {type: String},
+        startDate: {type:String, required: true},
+        endDate: {type: Date}, // can be null if still employed
+        responsibilities: [String] // bullet points
+        }
+    ],
+    skills: {
+        type: [String],
+        default: [] // default if empty
+    },
+    certifications: {
+        name: {type: String, required: true},
+        issuer: String,
+        date: Date,
+    },
+    awards: {
+        title: {type: String, required: true},
+        issuer: String,
+        date: Date,
+        details: String,
+    }
 })
